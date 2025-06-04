1fdbef6d351362227a5e70e5a0a0fe2a2fe846f1
adam-kodja <kodjaadam05@gmail.com>
2025-06-04 16:37:50 +0200

    ajout d'une table genre, et les films ont desormai une liste de nom de genres

--- backend/datasource.js
+++ backend/datasource.js
@@ -1,11 +1,15 @@
 import { DataSource } from 'typeorm';
+import Genre from './entities/genre.js';
+import Movie from './entities/movie.js';
+import User from './entities/user.js';
+import Rating from './entities/rating.js';
 import 'dotenv/config';
 
 export const appDataSource = new DataSource({
   type: 'sqlite',
   database: process.env.DATABASE_NAME,
   synchronize: false,
-  entities: ['entities/*.js'],
+  entities: [Genre, Movie, User, Rating],
   migrations: ['migrations/*.js'],
   cli: {
     migrationsDir: 'migrations',
