de963b6564e183a5dc0ea9f34a0d0c5f933b3316
adam-kodja <kodjaadam05@gmail.com>
2025-06-05 16:31:02 +0200

    ajout de keywords dans les colonnes de movies, et mise a jour de seed_movies

--- backend/scripts/seed_movies.js
+++ backend/scripts/seed_movies.js
@@ -1,17 +1,27 @@
 import { appDataSource } from '../datasource.js';
 import Movie from '../entities/movie.js';
-import Genre from '../entities/genre.js';
 import axios from 'axios';
 
 const options = {
   method: 'GET',
   url: 'https://api.themoviedb.org/3/movie/popular',
-  params: {language: 'en-US', page: '1'},
+  params: {language: 'en-US', page: '1'},//modifier la page pour ajouter d'autres films à la db
   headers: {
     accept: 'application/json',
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjlmNjAwMzY4MzMzODNkNGIwYjNhNzJiODA3MzdjNCIsInN1YiI6IjY0NzA5YmE4YzVhZGE1MDBkZWU2ZTMxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Em7Y9fSW94J91rbuKFjDWxmpWaQzTitxRKNdQ5Lh2Eo'
   }
 };
+
+function get_keyword_options (movie_id){
+  return {method: 'GET',
+  url: `https://api.themoviedb.org/3/movie/${movie_id}/keywords`,
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZjlmNjAwMzY4MzMzODNkNGIwYjNhNzJiODA3MzdjNCIsInN1YiI6IjY0NzA5YmE4YzVhZGE1MDBkZWU2ZTMxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Em7Y9fSW94J91rbuKFjDWxmpWaQzTitxRKNdQ5Lh2Eo'
+  }
+}
+};
+
 
 var movies = [];
 
@@ -31,12 +41,14 @@ async function get_movies() {
         const result = res.data;
         const n = result['results'].length;
         for (let i = 0; i < n; i++) {
+            
             movies.push({
                 id : result['results'][i]['id'],
                 title : result['results'][i]['title'],
                 date : result['results'][i]['release_date'],
                 genre: result['results'][i]['genre_ids'],
                 description: result['results'][i]['overview'],
+                keywords:'',
                 poster_path : 'https://image.tmdb.org/t/p/w500' + result['results'][i]['poster_path']
             });
         }
@@ -49,12 +61,47 @@ async function seed() {
   const movieRepository = appDataSource.getRepository(Movie);
 
   for (const movie of movies) {
-    try {
-      await movieRepository.insert(movie);
-      console.log(`Ajouté : ${movie.title}`);
-    } catch (err) {
-      console.error(`Erreur pour ${movie.title} :`, err.message);
+    const is_in_db = await movieRepository.findOneBy({ id: movie.id });
+    const has_keywords = is_in_db && is_in_db.keywords && is_in_db.keywords.trim() !== '';
+
+    if (has_keywords===null){
+
+      //appel a l'api pour modifier keywords
+      try {
+        const res = await axios.request(get_keyword_options(movie.id));
+        const result = res.data;
+        const n = result['keywords'].length;
+        for (let i = 0; i < n; i++) {
+          movie.keywords += result['keywords'][i]['name'] + ' ';
+        }
+      } catch (err) {
+        console.error(`Erreur lors de la récupération des keywords pour ${movie.title} :`, err.message);
+      }
+
+
+      if (is_in_db){
+        
+        try {
+          await movieRepository.update({ id: movie.id }, { keywords: movie.keywords });
+          console.log(`Mise à jour keywords pour : ${movie.title}`);
+        } catch (err) {
+          console.error(`Erreur de mise à jour pour ${movie.title} :`, err.message);
+        }
+      }
+      else {
+        try {
+          await movieRepository.insert(movie);
+          console.log(`Ajouté : ${movie.title}`);
+        } catch (err) {
+          console.error(`Erreur pour ${movie.title} :`, err.message);
+        }
+      }
     }
+    else {
+      console.log(`Déjà présent et à jour : ${movie.title}`)
+    }
+    
+    
   }
 }
 
